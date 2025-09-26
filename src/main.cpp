7fea8cfd96265c3a86b73847eab295a128b9e1eb
JernejL123 <jernej.lobnik@gmail.com>
2025-09-26 18:17:46 +0200

    koda za tehtanje v main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,57 +1,61 @@
-/*
-  Example from WiFi > WiFiScan
-  Complete details at https://RandomNerdTutorials.com/esp32-useful-wi-fi-functions-arduino/
-*/
+#include "HX711.h"
 
-#include "WiFi.h"
+#define DOUT  23
+#define CLK  19
+HX711 cell;
+const int LOADCELL_DOUT_PIN = 26;
+const int LOADCELL_SCK_PIN = 27;
 
-void initWiFi() {
-  WiFi.mode(WIFI_STA);
-  WiFi.begin("A1-0EE5E0", "8UUNQN8P8G");
-  Serial.print("Connecting to WiFi ..");
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print('.');
-    delay(1000);
-  }
-  Serial.println(WiFi.localIP());
-}
+const long LOADCELL_OFFSET = 50682624;
+const long LOADCELL_DIVIDER = 5895655;
 
+ 
+// Your WiFi credentials.
+// Set password to "" for open networks.
+ 
+float weight; 
+float calibration_factor = 14068.8; // for me this vlaue works just perfect 211000
+ 
+void measureweight();
 void setup() {
+  // Set up serial monitor
+  // Set up serial monitor
+  // Set up serial monitor
+  cell.begin(LOADCELL_DOUT_PIN,LOADCELL_SCK_PIN);
   Serial.begin(115200);
+  Serial.println("HX711 calibration sketch");
+  Serial.println("Remove all weight from scale");
+  Serial.println("After readings begin, place known weight on scale");
+  Serial.println("Press + or a to increase calibration factor");
+  Serial.println("Press - or z to decrease calibration factor");
+  cell.set_scale();
+  cell.tare(); //Reset the scale to 0
+  long zero_factor = cell.read_average(); //Get a baseline reading
+  Serial.print("Zero factor: "); //This can be used to remove the need to tare the scale. Useful in permanent scale projects.
+  Serial.println(zero_factor);
+ 
 
-  // Set WiFi to station mode and disconnect from an AP if it was previously connected
-  WiFi.mode(WIFI_STA);
-  WiFi.disconnect();
-  delay(100);
-
-  Serial.println("Setup done");
 }
-
-void loop() {
-  Serial.println("scan start");
-
-  // WiFi.scanNetworks will return the number of networks found
-  int n = WiFi.scanNetworks();
-  Serial.println("scan done");
-  if (n == 0) {
-      Serial.println("no networks found");
-  } else {
-    Serial.print(n);
-    Serial.println(" networks found");
-    for (int i = 0; i < n; ++i) {
-      // Print SSID and RSSI for each network found
-      Serial.print(i + 1);
-      Serial.print(": ");
-      Serial.print(WiFi.SSID(i));
-      Serial.print(" (");
-      Serial.print(WiFi.RSSI(i));
-      Serial.print(")");
-      Serial.println((WiFi.encryptionType(i) == WIFI_AUTH_OPEN)?" ":"*");
-      delay(10);
+ void loop() {
+  measureweight();
+}
+ 
+void measureweight(){
+  cell.set_scale(calibration_factor); //Adjust to this calibration factor
+  Serial.print("Reading: ");
+  weight = cell.get_units(5); 
+    if(weight<0)
+  {
+    weight=0.00;
     }
-  }
-  Serial.println("");
-  initWiFi();
-  Serial.print("RRSI: ");
-  Serial.println(WiFi.RSSI());
+  //Serial.print(scale.get_units(), 2);
+ // Serial.print(" lbs"); //Change this to kg and re-adjust the calibration factor if you follow SI units like a sane person
+  Serial.print("Kilogram:");
+  Serial.print( weight); 
+  Serial.print(" Kg");
+  Serial.print(" calibration_factor: ");
+  Serial.print(calibration_factor);
+  Serial.println();
+  // Delay before repeating measurement
+  delay(100);
 }
